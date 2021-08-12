6470fae9026002ee87352b3e3d5a7d7760470438
DLKARPAK <DLKARPAK@outlook.com>
2021-08-12 16:29:47 +0500

    Folders added in Application Layer

--- src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -9,10 +9,10 @@ namespace Ordering.Domain.Common
     //This class would have the common entities
     public abstract class EntityBase
     {
-        public int Id { get; set; }
+        public int Id { get; protected set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public string LastModifiedBy { get; set; }
-        public DateTime LastModifiedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
     }
 }
