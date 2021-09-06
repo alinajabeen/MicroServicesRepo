f460c1151171cbf11ddc78f281fb03cd09ad27d0
DLKARPAK <DLKARPAK@outlook.com>
2021-09-06 15:29:48 +0500

    Ordering API Code Completed

---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //It will check the profile information (Is there any class inherited from Profile and executing these profiles when running the application)
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //It will looking for the object inherited from abstract validator class
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            //mediatr will look for the classes which are inherited from IRequest and IRequestHandler
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            //Now we are adding the Pipeline Behaviours
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            return services;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours
+{
+    /// <summary>
+    /// This class runs all the validation classes if exist and collect the results, if there is any invalidate result then we are going to throw
+    /// custom validation exception. It is searching fluent validator object and abstract validator object and run all the validators before executing the request
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<IRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<IRequest>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+                if (failures.Count != 0)
+                    throw new ValidationException(failures);
+            }
+            return await next();
+        }
+    }
+}
