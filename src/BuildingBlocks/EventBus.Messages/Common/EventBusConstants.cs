eb7d2a9764bdf84747ca9d258e3a26b25f4fabc4
DLKARPAK <DLKARPAK@outlook.com>
2021-09-09 11:18:38 +0500

    Project Refernence Added (in Basket API) of Event Bus Mass Transit Packages added

---
+++ src/BuildingBlocks/EventBus.Messages/Common/EventBusConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Common
+{
+    public static class EventBusConstants
+    {
+        //Queue Name
+        public const string BasketCheckoutQueue = "basketcheckout-queue";
+    }
+}
